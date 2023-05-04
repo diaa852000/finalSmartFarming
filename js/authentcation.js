c3b2bc506a4a5d53fe10177ef2832d13ccd34c65
diaa <diaa2442000@gmail.com>
2023-05-04 17:38:09 +0300

    adding backGround to auth page, make it responsive

--- js/authentcation.js
+++ js/authentcation.js
@@ -1,6 +1,11 @@
 const signUpBtn = document.querySelector('#sign-up');
 const signInBtn = document.querySelector('#sign-in');
 const main = document.querySelector('#main');
+const toSignUpBtn = document.querySelector('.txt-sm-signup');
+const toSignInBtn =document.querySelector('.back-signin');
+const authentcation = document.querySelector('.auth-container')
+const signUp = document.querySelector('.sign-up');
+const signIn = document.querySelector('.sign-in');
 
 signUpBtn.addEventListener('click', ()=>{
     main.classList.add('right-panel-active');
@@ -9,3 +14,11 @@ signUpBtn.addEventListener('click', ()=>{
 signInBtn.addEventListener('click', ()=>{
     main.classList.remove('right-panel-active')
 });
+
+toSignUpBtn.addEventListener('click', ()=>{
+    authentcation.classList.add('toggle-auth');
+})
+
+toSignInBtn.addEventListener('click', ()=>{
+    authentcation.classList.remove('toggle-auth');
+})
